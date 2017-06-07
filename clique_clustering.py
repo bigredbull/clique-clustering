4aae8339e7ee4852d4d9a11bf7b2fc1711efe77b
Siegmeyer <herkaiwo@gmail.com>
2017-06-08 00:54:10 +0200

    Small clean-up

--- clique_clustering.py
+++ clique_clustering.py
@@ -1,53 +1,23 @@
-import community
 import networkx as nx
-import matplotlib.pyplot as pl
-from copy import deepcopy
 
 
-class Status(object):
+class Status:
     def __init__(self):
-        self.com2nodes = dict([])
-        self.node2com = dict([])
-        self.degrees = dict([])
-        self.gdegrees = dict([])
-        self.internals = dict([])
-        self.loops = dict([])
+        self.com2nodes = {}
+        self.internals = {}
+        self.node2com = {}
+        self.degrees = {}
         self.n_edges = 0
 
-    def __str__(self):
-        return ("node2com:"
-                + str(self.node2com)
-                + '\ncom2nodes: '
-                + str(self.com2nodes)
-                + "\ndegrees: "
-                + str(self.degrees)
-                + "\ninternals: "
-                + str(self.internals))
-
-    def copy(self):
-        '''
-           Perform a deep copy of status.
-        '''
-        new_status = Status()
-        new_status.node2com = self.node2com
-        new_status.com2nodes = self.com2nodes
-        new_status.internals = self.internals
-        new_status.degrees = self.degrees
-        new_status.gdegrees = self.gdegrees
-        new_status.n_edges = self.n_edges
-        new_status.com_mods = self.com_mods
-        return new_status
-
     def init(self, graph, part=None):
-        count = 0
-        self.com2nodes = dict([])
-        self.node2com = dict([])
-        self.degrees = dict([])
-        self.gdegrees = dict([])
-        self.internals = dict([])
-        self.loops = dict([])
         self.n_edges = graph.size()
+        self.internals = {}
+        self.com2nodes = {}
+        self.node2com = {}
         self.com_mods = {}
+        self.degrees = {}
+        
+        count = 0
 
         for node in graph.nodes():
             self.node2com[node] = count
@@ -59,16 +29,16 @@ class Status(object):
             self.com2nodes[count].add(node)
 
             deg = float(graph.degree(node))
+            
             if deg < 0:
                 error = "Bad graph type ({})".format(type(graph))
                 raise ValueError(error)
+            
             self.degrees[count] = deg
-            self.gdegrees[node] = deg
-            edge_data = graph.get_edge_data(node, node, {})
-            self.loops[node] = 0
             self.internals[count] = 0
             count += 1
 
+            
 class State:
     def __init__(self):
         self.com_t = None
@@ -96,19 +66,13 @@ class State:
         for v in self.moved:
             status.node2com[v] = self.com_s
             
-#@profile        
-def merge_coms(l_comA, l_comB, G, status, state=None, verb=False):
-    if verb:
-       print('\n\nMerging: {} + {}'.format(l_comA, l_comB))
-    
+
+def merge_coms(l_comA, l_comB, G, status, state=None):
     if l_comA == l_comB:
         return l_comA
 
     comA = status.com2nodes[l_comA]
     comB = status.com2nodes[l_comB]
-
-    if verb:
-       print('Communities:\n1: {}\n2: {}'.format(comA, comB))
 
     if len(comA) > len(comB):
         target = comA
@@ -158,72 +122,53 @@ def merge_coms(l_comA, l_comB, G, status, state=None, verb=False):
     return t_label
 
 
-def cliq2com(cliq, G, status):
-    coms = list(set([status.node2com[v] for v in cliq]))
-    new_com = coms[0]
+def cliq2com(G, clique, status):
+    '''
+       Merge clique into community. 
+       This method takes set of points and merges them 
+       into community, updating status along the way.
+       -----------------------------------------------
+       Parmeters:
+         [clique]: set of vertices
+         [G]: NetworkX's DiGraph
+         [status]: graph's Status
+    '''
+    
+    communities = list(set([status.node2com[v] for v in clique]))
+    last = communities[0]
 
-    for c in coms:
-        new_com = merge_coms(new_com, c, G, status)
+    for com in communities:
+        last = merge_coms(last, com, G, status)
 
-    return new_com
+    return last
 
         
-#@profile
-def modularity(status, verbose=0, changed=None):
-    changed = changed if changed else set(status.node2com.values())
+def modularity(status, changed=None):
+    '''
+        (Re)calculate modularity.
+    '''
+    
+    if not changed:
+        changed = set(status.node2com.values())
+        
     links = float(status.n_edges)
     result = 0.
-
-    if verbose:
-        print('\n     Calculating modularity...')
-        print('     Number of edges in a graph: {}'.format(links))
-
-#    for community in set(status.node2com.values()):
-#        in_degree = status.internals.get(community, 0.)
-#        degree = status.degrees.get(community, 0.)
-#
-#        if verbose > 1:
-#            print('     Community: {}, in-degree: {}, degree: {}'.format(community, in_degree, degree))
-#
-#        if links > 0:
-#            result += in_degree / links - ((degree / (2. * links)) ** 2)
         
     for community in changed:
-        in_degree = status.internals.get(community, 0.)
         degree = status.degrees.get(community, 0.)
-        com_mod = 0
+        in_degree = status.internals.get(community, 0.)
+        status.com_mods[community] = in_degree / links - ((degree / (2. * links)) ** 2)
 
-        if verbose > 1:
-            print('     Community: {}, in-degree: {}, degree: {}'.format(community, in_degree, degree))
-
-        if links > 0:
-            com_mod += in_degree / links - ((degree / (2. * links)) ** 2)
-
-        status.com_mods[community] = com_mod
-
-#    if len(status.com_mods.keys()) != len(status.com2nodes.keys()):
-#        raise Exception('Mismatch: {} /= {}'.format(status.com_mods.keys() , status.com2nodes.keys()))
-
-    if verbose:
-        print('     Result: {}\n'.format(result))
-
-    modularity = sum(status.com_mods.values())
-
-#    if abs(modularity - result) > 0.000001:
-#        print("ERROR: {} /= {}".format(modularity, result))
-
-    return modularity
+    return sum(status.com_mods.values())
 
 
-#@profile
 def cluster(G):
     max_mod = -1
     touched = set()
     status = Status()
     status.init(G)
-    _status = status.copy()
 
-    polys = [frozenset(c) for c in nx.find_cliques(G) if len(c) > 1]
+    polys = [frozenset(c) for c in nx.find_cliques(G) if len(c) > 2]
     polys.sort(key=lambda p: -len(p))
 
     print('Phase 1... {} cliques'.format(len(polys)))
@@ -239,69 +184,41 @@ def cluster(G):
 
         touched = touched.union(p)
 
-        new_com = cliq2com(p, G, _status)
-        new_mod = modularity(_status, 0, [new_com])
+        new_com = cliq2com(G, p, status)
+        new_mod = modularity(status, [new_com])
 
         if new_mod > max_mod:
             max_mod = new_mod
-            status = _status.copy()
+            status = status
         else:
             continue
 
-    min_del = 0.0000001
     merged = set()
-    used = set()
+    visited_pairs = set()
     coms = set(status.node2com.values())
-    count = 0
 
     print('Phase 2... {} communities'.format(len(coms)))
 
     for com in coms:
-        # Check if community exists.
         if com in merged:
             continue
 
         for com_ in coms:
-            if com == com_ or com_ in merged or frozenset([com, com_]) in used:
+            if com == com_ or com_ in merged or frozenset([com, com_]) in visited_pairs:
                 continue
 
-            #print('\n{}'.format(_status))
+            visited_pairs.add(frozenset([com, com_]))
 
-            used.add(frozenset([com, com_]))
-            count += 1
-
-            #print('Merging: {} + {}'.format(com, com_))
-
-            _status = status
             state = State()
-            #print('\nBefore merge: {}'.format(_status))
-            new_com = merge_coms(com, com_, G, _status, state, verb=False)
-            new_mod = modularity(_status, 0, [new_com])
+            new_com = merge_coms(com, com_, G, status, state)
+            new_mod = modularity(status, [new_com])
 
             if new_mod > max_mod:
                 max_mod = new_mod
-                status = _status
-                merged.add(com + com_ - new_com)
-                #print('\nMerged: {} + {}'.format(com, com_))
-
-                if False and min_del > (new_mod - max_mod):
-                    print('Merged {} times.'.format(count))
-                    print('Final modularity: {}'.format(modularity(status, 0)))
-                    # print('Final partition: {}'.format(relabel(status.node2com)))
-                    return (status.node2com, G)
-                
+                merged.add(com + com_ - new_com)               
                 break
             else:
-                #print('Merged {} times.'.format(count))
-                #print('Final modularity: {}'.format(modularity(status, 0)))
-                #return (status.node2com, G)
-                
-                #print('After merge: {}'.format(_status))
-                #print('Reversing... ')
-                state.reverse(_status)
-                #print('After reverse: {}'.format(_status))
-                
-    print('Merged {} times.'.format(count))
+                state.reverse(status)
+    
     print('Final modularity: {}'.format(modularity(status, 0)))
-    # print('Final partition: {}'.format(relabel(status.node2com)))
-    return (status.node2com, G)
+    return status.node2com
